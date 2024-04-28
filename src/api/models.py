3dff29a8f38733030cff93265baa2fc3369f0a93
Irene VS <147383929+iireneVS@users.noreply.github.com>
2024-04-28 21:18:30 +0000

    feat: models de Films, Places y Travels

--- src/api/models.py
+++ src/api/models.py
@@ -51,4 +51,62 @@ class Customers(db.Model):
                 'address': self.address,
                 'phone': self.phone,
                 'id_type': self.id_type,
-                'id_number': self.id_number}          
+                'id_number': self.id_number}   
+
+
+class Films(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    director = db.Column(db.String(120))
+    description = db.Column(db.String(800))
+
+    def __repr__(self):
+        return f'<Films {self.id} name: {self.name}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'director': self.director,
+                'description': self.description}   
+
+
+class Places(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    film_id = db.Column(db.String(120), db.ForeignKey('films.id'))
+    country = db.Column(db.String(120), nullable=False)
+    img_url = db.Column(db.String(120))  
+
+    def __repr__(self):
+        return f'Places {self.id} country: {self.country}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'title': self.title,
+                'film_id': self.film_id,
+                'country': self.country,
+                'img_url': self.img_url}                       
+
+
+class Travels(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    days = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    description = db.Column(db.String(500), nullable=False)
+    place_id = db.Column(db.String(120), db.ForeignKey('places_id'))
+    img_url = db.Column(db.String)
+    itinerary = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Travels {self.id} name: {self.name}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'days': self.days,
+                'price': self.price,
+                'description': self.description,
+                'place_id': self.place_id,
+                'img_url': self.img_url,
+                'itinerary': self.itinerary}    
