ebe09092c14d536afa659cd44c774ac84a0ef070
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2024-05-03 12:58:00 +0000

    feat: routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from api.models import db, Users, Admins, Customers, Films, Places, Travels, Departures, ShoppingCart
-
+# from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies, set_access_cookies
+from sqlalchemy import func 
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 CORS(api)  # Allow CORS requests to this API
@@ -55,6 +57,72 @@ def handle_login():
                      'results': results}
     return response_body, 200
 
+@api.route('/signup', methods=["POST"])
+def handle_signup():
+    data = request.get_json()
+    response_body = {}
+    try: 
+        email = data['user']['email'].lower()
+    except:
+        response_body['message'] = 'user.email is empty or wrong'
+        return response_body, 400
+    # Verificamos si el usuario ya existe
+    is_user = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
+    if is_user:
+        response_body['message'] = 'The email is registered'
+        return response_body, 403
+    # Almacenamos en memoria los datos de user y admin (member, se debe crear desde POST /member)
+    data_user = data['user']
+    data_admin = data.get('admin', None)
+    # Si es admin, verificamos que no exista (por el name y el user_id)
+    if data_admin:
+        try:
+            name = data['admin']['name'].lower()
+            user_id = data['admin']['user_id']
+        except:
+            response_body['message'] = ' admin.name o admin.id  is empty or wrong'
+            return response_body, 400
+        is_name = db.session.execute(db.select(Admins).where(func.lower(Admins.name) == name)).scalar()
+        is_user_id = db.session.execute(db.select(Admins).where(func.lower(Admins.user_id) == id)).scalar()
+        if is_name or is_user_id:
+            response_body['message'] = 'The name or nif is registered'
+            return response_body, 403
+    # Si la estructura del JSON es incorrecta, no se podrán cargar los datos
+    try:
+        user = Users(email=data_user['email'], 
+                     password=data_user['password'], 
+                     is_active=True)
+        db.session.add(user)
+        db.session.commit()
+        results = {'user': user.serialize(),
+                   'member': None,
+                   'advis': None}
+        if data_advisor:
+            advisor = Admins(name=data_advisor['name'],  
+                               is_active=True,
+                               user_id=user.id)
+            db.session.add(advisor)
+            db.session.commit()
+            results['admin'] = advisor.serialize()
+        access_token = create_access_token(identity=[user.id,
+                                                     None,
+                                                     admin.id if data_admin else None])
+        response_body = {'message': 'User created',
+                         'token': access_token,
+                         'results': results}
+    except:
+        response_body = {'message': "Bad JSON structure"}
+        return response_body, 400
+    return response_body, 201
+
+
+@api.route('/logout', methods=["POST"])
+@jwt_required()
+def handle_logout():
+    user_id = get_jwt_identity()[0]  # Obtén el ID del usuario a partir del token
+    unset_jwt_cookies()  # Revoca el token actual para deshabilitarlo
+    response_body = {'message': 'Logout successful'}
+    return response_body, 200
 
 @api.route('/users', methods=['GET'])  # El POST se realiza en /login
 @jwt_required() 
@@ -190,14 +258,83 @@ def handle_admin_id(admin_id):
         return response_body, 200
 
 @api.route('/travels', methods=['GET'])
+def handle_travels():
+    travels = db.session.execute(db.select(Travels)).scalars()
+    response_body = {'message': 'Travels', 
+                     'results': Travels}
+    return response_body, 200 
 
 @api.route('/places', methods=['GET'])
+def handle_places(): 
+    places = db.session.execute(db.select(Places)).scalars()
+    response_body = {'message': 'Places', 
+                      'results': Places}
+    return response_body, 200 
+
 
 @api.route('/films', methods=['GET'])
+def handle_films(): 
+    films = db.session.execute(db.select(Films)).scalars()
+    response_body = {'message': 'Films', 
+                     'results': Films}
+    return response_body, 200 
 
 @api.route('/departures', methods=['GET'])
+def handle_departures(): 
+    departures = db.session.execute(db.select(Departures)).scalars()
+    response_body = {'message': 'Departures', 
+                     'results': Departures}
+    return response_body, 200 
 
-@api.route('/shopping-cart', methods= ['GET', 'POST'])
+@api.route('/shoppingcart', methods= ['GET'])
+@jwt_required()
+def handle_shoppingcart():
+    identity = get_jwt_identity()
+    if request.method == 'GET' and identity[1]:
+        carts = db.session.execute(db.select(ShoppingCart)).scalars()
+        cart_list = [cart.serialize() for cart in carts]
+        response_body = {'message': 'ShoppingCart',
+                         'results': cart_list}
+        return response_body, 200 
+
+@api.route('/shoppingcart', methods= ['POST']) 
+@jwt_required()
+def handle_shoppingcart():
+    identity = get_jwt_identity()
+    if request.method == 'POST' and member_id:
+        cart = db.session.execute(db.select(ShoppingCarts).where(ShoppingCarts.member_id == member_id)).scalar()
+        results = {}
+        if not cart:
+            # Verificamos si ya tiene un carrito
+            cart = ShoppingCart(price=
+                                departure_id = 
+                                customer_id = 
+                                payment = 
+                                passengers =)
+            db.session.add(shoppingcart)
+            db.session.commit()
+        data = request.get_json()
+        cart_item = ShoppingCart (price=data['price'],
+                                  departure_id = data ['departures.id'],
+                                  customer_id = data['customers.id'],
+                                  payment = data ['payment'],
+                                  passengers = data ['passengers'])
+        db.session.add(shoppingcart)
+        db.session.commit()
+        # Actualizamos el monto total del carrito
+        cart['payment'] += cart_item['price'] * cart_item['passengers']
+        db.session.commit()
+        results['shopping_cart'] = cart.serialize()
+        cart_items = db.session.execute(db.select(ShoppingCart)).scalars()
+        list_items = []
+        for item in cart_items:
+            list_items.append(item.serialize())
+        results['shoppingcart'] = list_items
+        response_body = {'message': 'Shopping Cart with all items', 
+                         'results': results}
+        return response_body, 201 
+    response_body = {'message': "Restricted access"}
+    return response_body, 401
 
 # @api.route('/payment')
 
